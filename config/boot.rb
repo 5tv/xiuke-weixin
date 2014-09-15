e469a946779a0849d68f44f744460a07360fca05
徐德科 <xudeke@Macintosh-4.local>
2014-09-15 17:43:25 +0800

    bug_modified

--- config/boot.rb
+++ config/boot.rb
@@ -12,6 +12,7 @@ WX_ACCOUNT = YAML.load_file(File.expand_path("#{PADRINO_ROOT}/config", __FILE__)
 MENU = YAML.load_file(File.expand_path("#{PADRINO_ROOT}/config", __FILE__) + '/menu_config.yml')[RACK_ENV]
 #app id and secret
 Weixin::Menu.new(WX_ACCOUNT['appid'],WX_ACCOUNT['appsecret']).add(MENU)
+WEIXIN_CLIENT = Weixin::Client.new(WX_ACCOUNT['appid'],WX_ACCOUNT['appsecret'])
 ##
 # ## Enable devel logging
 #
