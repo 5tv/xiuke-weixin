7a7729778036f4eed02508084e72faaf2627ca75
徐德科 <xudeke@Macintosh-4.local>
2014-09-18 11:58:45 +0800

    redirect_modified

--- app/app.rb
+++ app/app.rb
@@ -170,13 +170,7 @@ module Weixin2
             new_message = msg_router(message) unless message.nil?
             new_message
         end
-
-        get '/redirect' do
-            # content_type :xml, 'charset' => 'utf-8'
-            # message = request.env[Weixin::Middleware::WEIXIN_MSG]            
-            # redirect "http://5tv.com/bind?weixin_openid=#{message.FromUserName}"
-            redirect "http://5tv.com/"
-        end
+        
     end
 
   end
