611bb63cf18548f2b96d5309e68dbddd2dacfb12
xudeke <xudeke@5tv.com>
2015-01-29 11:50:39 +0800

    many games added

--- app/app.rb
+++ app/app.rb
@@ -3,6 +3,7 @@ module Weixin2
     register Padrino::Mailer
     register Padrino::Helpers
     enable :sessions
+    set :public_folder, '/public'
     use Weixin::Middleware, WX_ACCOUNT['token'], '/' 
     configure do
       set :wx_id, WX_ACCOUNT['wx_account_id']
