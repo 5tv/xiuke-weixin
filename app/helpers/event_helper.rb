bddfa8b8dd4b4d6cf3c4edf4b5a71fea014fdf10
happybai <happybyronbai@gmail.com>
2015-11-06 18:28:21 +0800

    Fix

--- app/helpers/event_helper.rb
+++ app/helpers/event_helper.rb
@@ -15,8 +15,8 @@ module Weixin2
           event_location(msg) 
         when 'SCAN'
           Weixin.text_msg(msg.ToUserName, msg.FromUserName, '扫码啦！！我是天才')
-          event_scan(msg)
-          Weixin.text_msg(msg.ToUserName, msg.FromUserName, "OpenId: #{msg.FromUserName} msg: #{msg.EventKey}")
+          # event_scan(msg)
+          # Weixin.text_msg(msg.ToUserName, msg.FromUserName, "OpenId: #{msg.FromUserName} msg: #{msg.EventKey}")
           # event_scan(msg)
           # Weixin.text_msg(msg.ToUserName, msg.FromUserName, '扫码啦！！我是天才')       
         else
