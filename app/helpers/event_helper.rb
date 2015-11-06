2b79047b62e7fd8858c86c75f1b05b075aa711c0
happybai <happybyronbai@gmail.com>
2015-11-06 15:56:13 +0800

    Debug

--- app/helpers/event_helper.rb
+++ app/helpers/event_helper.rb
@@ -14,7 +14,7 @@ module Weixin2
         when 'LOCATION'
           event_location(msg) 
         when 'SCAN'
-
+          event_scan(msg)
           Weixin.text_msg(msg.ToUserName, msg.FromUserName, '扫码啦！！我是天才')       
         else
           Weixin.text_msg(msg.ToUserName, msg.FromUserName, '未知事件')
