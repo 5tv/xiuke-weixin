3e412b8766e978c9a0bbe83442f608fca8c9f10c
happybai <happybyronbai@gmail.com>
2015-11-07 11:51:18 +0800

    Debug

--- app/helpers/event_helper.rb
+++ app/helpers/event_helper.rb
@@ -41,9 +41,9 @@ module Weixin2
       def event_scan(msg)
         scene_id = msg.EventKey
         open_id = msg.FromUserName
-        # result = CACHE.read("/weixin_follow/#{scene_id}")
-        # obj = JSON.parse(result)
-        # Weixin.text_msg(msg.ToUserName, msg.FromUserName, "video_id: #{obj['video_id']} timepoint: #{obj['timepoint']}")
+        result = CACHE.read("/weixin_follow/#{scene_id}")
+        obj = JSON.parse(result)
+        Weixin.text_msg(msg.ToUserName, msg.FromUserName, "video_id: #{obj['video_id']} timepoint: #{obj['timepoint']}")
         # send_video_message(open_id, obj['video_id'], obj['timepoint'])
         Weixin.text_msg(msg.ToUserName, msg.FromUserName, "OpenId: #{msg.FromUserName} scene_id: #{msg.EventKey} ticket: #{msg.Ticket}")
       end
