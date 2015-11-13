7de857eca01e7f457f5f7cb4d264096e06e65458
happybai <happybyronbai@gmail.com>
2015-11-13 14:02:43 +0800

    change config

--- config/boot.rb
+++ config/boot.rb
@@ -14,15 +14,20 @@ WX_ACCOUNT = YAML.load_file(File.expand_path("#{PADRINO_ROOT}/config", __FILE__)
 MENU = YAML.load_file(File.expand_path("#{PADRINO_ROOT}/config", __FILE__) + '/menu_config.yml')[RACK_ENV]
 Weixin::Menu.new(WX_ACCOUNT['appid'],WX_ACCOUNT['appsecret']).add(MENU)
 WEIXIN_CLIENT = Weixin::Client.new(WX_ACCOUNT['appid'],WX_ACCOUNT['appsecret'])
-if RACK_ENV == 'production'
-  UPHOST = '5tv.com'
-  LocalServer = '106.187.35.209'
-  ApiServer = 'api.5tv.com'
-else
-  UPHOST =  'weixintest.5tv.com'
-  LocalServer = '104.237.155.77'
-  ApiServer '54.223.162.137'
-end
+#if RACK_ENV == 'production'
+  #UPHOST = '5tv.com'
+  #LocalServer = '106.187.35.209'
+  #APISERVER = 'api.5tv.com'
+#
+#else
+  #UPHOST =  'weixintest.5tv.com'
+  #LocalServer = '104.237.155.77'
+  #APISERVER '54.223.162.137'
+#end
+UPHOST = '54.223.162.137:9999'
+APISERVER = '54.223.162.137:9999'
+LocalServer = '104.237.155.77'
+
 REDIS_CONFIG = YAML.load_file(File.expand_path("#{PADRINO_ROOT}/config", __FILE__) + '/redis.yml')[RACK_ENV]
 CACHE = ActiveSupport::Cache::RedisStore.new :host => REDIS_CONFIG['host'], :driver => :hiredis, :expires_in => 1.hour
 ##
